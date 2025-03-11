f2cb2b4b40274fa508bd8e7d2abf9fac324a95dc
ho2ast <ho2ast@gmail.com>
2025-03-11 10:30:24 +0900

    hello playground

---
+++ src/test/java/com/ho2ast/playground/PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ho2ast.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
